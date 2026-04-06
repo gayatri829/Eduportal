145f7c6ad2d82dc3d24d9561ce7a4177764fff4f
gayatri829 <ku2507u0052@karnavatiuniversity.edu.in>
2026-04-06 08:54:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1030,4 +1030,4 @@ if __name__ == "__main__":
     print("  ➜   Registration : http://127.0.0.1:5000/")
     print("  ➜   View Records  : http://127.0.0.1:5000/students")
     print("="*55 + "\n")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
